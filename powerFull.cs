bd007ce3a021e9f94d75c2c91bcb1df359804003
Neutron3529 <qweytr_1@163.com>
2022-02-03 14:42:35 +0800

    Added translation.

--- powerFull.cs
+++ powerFull.cs
@@ -54,7 +54,7 @@ export FILE_NAME="$0"
   `[ -e "../${GAME_NAME}_Data/Managed/Assembly-CSharp-firstpass.dll" ] && echo "-r:\"../${GAME_NAME}_Data/Managed/Assembly-CSharp-firstpass.dll\""` \
   -out:'../BepInEx/plugins/'"${FILE_NAME%.*}".dll \
   -optimize \
-  - && ( rm "../BepInEx/config/Neutron3529.Cheat.cfg" 2>/dev/null )
+  - && rm -f "../BepInEx/config/Neutron3529.Cheat.cfg";
 
 if [ -n "$2" ]; then
     git add ${FILE_NAME}
@@ -91,7 +91,7 @@ using UnityEngine;
 
 namespace PowerFull
 {
-    [BepInPlugin("Neutron3529.Cheat", "PowerFull", "0.2.1")]
+    [BepInPlugin("Neutron3529.Cheat", "PowerFull", "0.2.2")]
     public class PowerFull : BaseUnityPlugin {
         public static float power_mul=10.0f;
 #if DEBUG
@@ -144,6 +144,18 @@ namespace PowerFull
                 logger("PowerFull-二周目矩阵支援-加载完成");
 #endif
             }
+            if ((Config.Bind<bool>("config", "CheckBuildConditions_water", true, "允许抽水站抽地下水，允许在水上建造建筑").Value)){
+                harmony.PatchAll(typeof(BuildTool_ClickBuildTool_BlueprintPasteCheckBuildConditions));
+#if DEBUG
+                logger("PowerFull-去除地形限制-加载完成");
+#endif
+            }
+            if ((Config.Bind<bool>("config", "inf_matrix", true, "特定物品的消耗改为虚空创造").Value)){
+                harmony.PatchAll(typeof(StorageComponentTakeTailItems));
+#if DEBUG
+                logger("PowerFull-特定物品的消耗改为虚空创造-加载完成");
+#endif
+            }
 #if DEBUG
             logger("PowerFull-加载完成");
 #endif
@@ -173,7 +185,7 @@ namespace PowerFull
                         matcher
                         .Advance(1) // Move cursor to after ldfld
                         .InsertAndAdvance(
-                            new CodeInstruction(OpCodes.Ldc_R4,10f),
+                            new CodeInstruction(OpCodes.Ldc_R4,50f),
                             new CodeInstruction(OpCodes.Mul)
                         )
                     ).InstructionEnumeration();
@@ -307,10 +319,10 @@ namespace PowerFull
                     randSeed=0;
                     nearestFactory = GameMain.mainPlayer.nearestFactory;
                     mecha = GameMain.mainPlayer.mecha;
-                    GameHistoryDataGainTechAwards.AddStats(1120,9000000);//氢
-                    GameHistoryDataGainTechAwards.AddStats(1121,2000000);//重氢
-                    GameHistoryDataGainTechAwards.AddStats(1126, 200000);//卡晶
-                    GameHistoryDataGainTechAwards.AddStats(1304, 200000);//阴间过滤器
+//                     GameHistoryDataGainTechAwards.AddStats(1120,9000000);//氢
+//                     GameHistoryDataGainTechAwards.AddStats(1121,2000000);//重氢
+//                     GameHistoryDataGainTechAwards.AddStats(1126, 200000);//卡晶
+//                     GameHistoryDataGainTechAwards.AddStats(1304, 200000);//阴间过滤器
                     GameHistoryDataGainTechAwards.AddItems(1202, 30+150-10);//磁线圈，升级用，有10个多余（因为我们已经支付了这一项科技的成本）
                     GameHistoryDataGainTechAwards.AddItems(1301, 40+220);//电路板，升级用
                     GameHistoryDataGainTechAwards.AddItems(1201, 20+  0);//  齿轮，升级用
@@ -321,26 +333,107 @@ namespace PowerFull
                     GameHistoryDataGainTechAwards.AddItems(1103,  0+120);//  钢材，升级用
                     GameHistoryDataGainTechAwards.AddItems(1030,  0+ 60);//  木材，升级用
                     GameHistoryDataGainTechAwards.AddItems(1109,  0+ 60);//高能石墨升级用
-
-                    GameHistoryDataGainTechAwards.AddItems(6001,400+1400);//初始升级用矩阵
-                    GameHistoryDataGainTechAwards.AddItems(6002,400+1400);
-                    GameHistoryDataGainTechAwards.AddItems(6003,300+1200);
-
-                    GameHistoryDataGainTechAwards.AddItems(2104,  10);//物流站，10个
-                    GameHistoryDataGainTechAwards.AddItems(2316,  10);//大型采矿机，10个
-                    GameHistoryDataGainTechAwards.AddItems(5002, 100);//大飞机，100个
-                    GameHistoryDataGainTechAwards.AddItems(2003, 300);//蓝带，300个
-
-
                     GameHistoryDataGainTechAwards.AddStats(1403,     60);//湮灭约束球
                     GameHistoryDataGainTechAwards.AddItems(1803, 120);//燃料棒
                     GameHistoryDataGainTechAwards.AddItems(2001,   1);//黄带，1个，用于治疗强迫症
+//
+//                     GameHistoryDataGainTechAwards.AddItems(6001,400+1400);//初始升级用矩阵
+//                     GameHistoryDataGainTechAwards.AddItems(6002,400+1400);
+//                     GameHistoryDataGainTechAwards.AddItems(6003,300+1200);
 
-                    GameHistoryDataGainTechAwards.AddTrashes(6001,1000,156+1);
-                    GameHistoryDataGainTechAwards.AddTrashes(6002,1000,157+1);
-                    GameHistoryDataGainTechAwards.AddTrashes(6003,1000,124+1);
-                    GameHistoryDataGainTechAwards.AddTrashes(6004,1000,90+1);
-                    GameHistoryDataGainTechAwards.AddTrashes(6005,1000,((int)67.5)+1);
+                    GameHistoryDataGainTechAwards.AddItems(2103,  40);//物流站，40个
+                    GameHistoryDataGainTechAwards.AddItems(2104,  10);//物流站，10个
+                    GameHistoryDataGainTechAwards.AddItems(2105,  40);//物流站，40个
+                    GameHistoryDataGainTechAwards.AddItems(2316,  20);//大型采矿机，20个
+                    GameHistoryDataGainTechAwards.AddItems(5001,3000);//小飞机，100个
+                    GameHistoryDataGainTechAwards.AddItems(5002, 100);//大飞机，100个
+                    GameHistoryDataGainTechAwards.AddItems(2003,3000);//蓝带，3000个
+
+
+
+//                     GameHistoryDataGainTechAwards.AddTrashes(6001,1000,156);
+//                     GameHistoryDataGainTechAwards.AddTrashes(6002,1000,157);
+//                     GameHistoryDataGainTechAwards.AddTrashes(6003,1000,124);
+//                     GameHistoryDataGainTechAwards.AddTrashes(6004,1000,90+1);
+//                     GameHistoryDataGainTechAwards.AddTrashes(6005,1000,((int)67.5)+1);
+                }
+            }
+        }
+        [HarmonyPatch(typeof(BuildTool_Click), "CheckBuildConditions")]
+        [HarmonyPatch(typeof(BuildTool_BlueprintPaste), "CheckBuildConditions")]
+        class BuildTool_ClickBuildTool_BlueprintPasteCheckBuildConditions {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+                return new CodeMatcher(instructions)
+                    .MatchForward(false, // false = move at the start of the match, true = move at the end of the match
+                        new CodeMatch(i => CodeInstructionExtensions.LoadsConstant(i,EBuildCondition.NeedWater) || CodeInstructionExtensions.LoadsConstant(i,EBuildCondition.NeedGround)),
+                        new CodeMatch(OpCodes.Stfld, AccessTools.Field(typeof(BuildPreview),"condition"))
+                    ).Repeat( matcher => matcher// Do the following for each match
+                        .SetOperandAndAdvance(
+                            (int)(EBuildCondition.Ok)
+                        )
+                    ).InstructionEnumeration();
+            }
+        }
+        [HarmonyPatch(typeof(StorageComponent), "TakeTailItems",new Type[]{typeof(int),typeof(int),typeof(int),typeof(bool)},new ArgumentType[]{ArgumentType.Ref,ArgumentType.Ref,ArgumentType.Out,ArgumentType.Normal})]
+        class StorageComponentTakeTailItems {
+            static PlanetFactory nearestFactory;
+            static Mecha mecha;
+            static Dictionary<int,int[,]> dict = new Dictionary<int,int[,]> {
+                {1101,new int [,]{{1001,1}}},//铁
+                {1102,new int [,]{{1001,1}}},//磁铁
+                {1104,new int [,]{{1002,1}}},//铜
+                {1202,new int [,]{{1102,1},{1104,1}}},//磁线圈，多耗费0.5铜
+                {1301,new int [,]{{1101,1},{1104,1}}},//电路板，多耗费0.5铜
+                {6001,new int [,]{{1202,1},{1301,1}}},//=磁线圈 电路板
+                {1109,new int [,]{{1006,2}}},//高能石墨
+                {6002,new int [,]{{1109,2},{1120,2}}},
+                {1112,new int [,]{{1109,1}}},//金刚石
+                {1106,new int [,]{{1004,2}}},//钛
+                {1118,new int [,]{{1106,3},{1117,1}}},//钛晶石
+                {6003,new int [,]{{1112,1},{1118,1}}},
+                {1105,new int [,]{{1003,2}}},//硅
+                {1302,new int [,]{{1105,2},{1104,1}}},//微晶元件
+                {1303,new int [,]{{1302,2},{1301,2}}},//处理器
+                {1124,new int [,]{{1015,1}}},//碳纳米管
+                {1113,new int [,]{{1105,1}}},//晶格硅
+                {1114,new int [,]{{1007,1}}},//精炼油，未计算副产物氢
+                {1115,new int [,]{{1114,2},{1109,1}}},//塑料
+                {1402,new int [,]{{1124,2},{1113,2},{1115,1}}},//粒子宽带
+                {6004,new int [,]{{1303,2},{1402,1}}},
+                {6005,new int [,]{{1126,1},{1304,1},{1305,1},{1305,1},{1209,1},{1127,1}}},//未完
+                {6006,new int [,]{{6001,1},{6002,1},{6003,1},{6004,1},{6005,1},{1122,1},{1208,1}}},
+            };
+            public static void AddItems(int id,int count){
+                if(dict.ContainsKey(id)){
+                    int[,] x=dict[id];
+                    var len=x.GetLength(0);
+                    for (int i = 0; i < len; i++){
+                        StorageComponentTakeTailItems.AddItems(x[i,0],count*x[i,1]);
+                        mecha.AddConsumptionStat(x[i,0],count*x[i,1],nearestFactory);
+                    }
+                }else{
+                    StorageComponentTakeTailItems.AddStats(id,count);
+                }
+                mecha.AddProductionStat(id,count,nearestFactory);
+            }
+            public static void AddStats(int id,int count){
+                mecha.AddProductionStat(id,count,nearestFactory);
+                mecha.AddConsumptionStat(id,count,nearestFactory);
+            }
+            public static bool Prefix(ref int itemId, ref int count) {
+                switch(itemId){
+                    case 6001 :
+                    case 6002 :
+                    case 6003 :
+                    case 6004 :
+                    case 6005 :
+                    case 6006 :
+                        nearestFactory = GameMain.mainPlayer.nearestFactory;
+                        mecha = GameMain.mainPlayer.mecha;
+                        StorageComponentTakeTailItems.AddStats(itemId,count);
+                        return false;
+                    default : /* 可选的 */
+                    return true;
                 }
             }
         }
